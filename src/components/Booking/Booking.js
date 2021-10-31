5a74384bbc11a3f8f7db4163bc8993c7df6ed611
Anowar Hossain <anwarsohan71@gmail.com>
2021-10-31 12:06:01 +0600

    last-commit

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
+import './Booking.css';
 
 const Booking = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { serviceId } = useParams();
+    const [book, setBook] = useState({});
+    const { user } = useAuth();
+    useEffect(()=>{
+        fetch(`http://localhost:5000/services/${serviceId}`)
+        .then(res=>res.json())
+        .then(data=>setBook(data));
+    },[])
     return (
-        <div>
-            <h1>This is Booking</h1>
+        <div className="row">
+            <h1 className="text">Booking Page</h1>
+            <div className="col-md-6">
+            <img className="w-50 h-50" src={book.img} alt="" />
+            <h3>{book.name}</h3>
+            <p><small>{book.description}</small></p>
+            </div>
+            <div className="col-md-6">
+            <form className="booking-form" onSubmit={handleSubmit()}>
+                <input defaultValue={user.displayName} {...register("name")} />
+                <input defaultValue={user.email} {...register("email", { required: true })} />
+                {errors.email && <span className="error">This field is required</span>}
+                <input placeholder="Address" defaultValue="" {...register("address")} />
+                <input placeholder="City" defaultValue="" {...register("city")} />
+                <input placeholder="phone number" defaultValue="" {...register("phone")} />
+                <input type="submit" value="Booked" />
+            </form>
+            </div>
         </div>
     );
 };
--- src/components/Services/Service/Service.js
+++ src/components/Services/Service/Service.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({service}) => {
-    const {_id, img, name, price, description} = service;
+    const {_id, img, name, price } = service;
     return (
         <div className="service p-2">
            <img src={img} alt="" />
            <h4>{name}</h4>
-           <p>{description}</p>
            <h2>price: {price}</h2>
            <Link to={`/booking/${_id}`}>
-                <button className="btn btn-warning">Book {name.toLowerCase()}</button>
+                <button className="btn btn-warning">Details {name.toLowerCase()}</button>
             </Link>
 
            {/* <Link to={`/details/${_id}`}>
